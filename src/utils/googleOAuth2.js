1be423067d4044c4ee5429164681966515f766ca
OlehKotov <mail.oleg456@gmail.com>
2025-09-19 23:10:53 +0300

    Update googleOAuth2.js

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -1,19 +1,14 @@
-
-import createHttpError from 'http-errors';
 import { OAuth2Client } from 'google-auth-library';
 import path from 'node:path';
-import { readFile } from 'fs/promises';
-
+import createHttpError from 'http-errors';
 import { getEnvVar } from './getEnvVar.js';
 
-const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
-
-const oauthConfig = JSON.parse(await readFile(PATH_JSON));
+const REDIRECT_URI = getEnvVar('GOOGLE_AUTH_REDIRECT_URI');
 
 const googleOAuthClient = new OAuth2Client({
   clientId: getEnvVar('GOOGLE_AUTH_CLIENT_ID'),
   clientSecret: getEnvVar('GOOGLE_AUTH_CLIENT_SECRET'),
-  redirectUri: oauthConfig.web.redirect_uris[0],
+  redirectUri: REDIRECT_URI,
 });
 
 export const generateAuthUrl = () =>
@@ -24,7 +19,7 @@ export const generateAuthUrl = () =>
     ],
   });
 
-  export const validateCode = async (code) => {
+export const validateCode = async (code) => {
   const response = await googleOAuthClient.getToken(code);
   if (!response.tokens.id_token) throw createHttpError(401, 'Unauthorized');
 
