e046ceb0667adab5f9e034336b55c9ae00fe17ac
OlehKotov <mail.oleg456@gmail.com>
2025-09-22 10:20:45 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -14,7 +14,12 @@ export const startServer = () => {
   const app = express();
 
   app.use(express.json());
-  app.use(cors());
+
+  app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true,
+}));
+
   app.use(cookieParser());
 
   app.use(
