1f7a92bdd72f6c8d2286109853886b2cb2ce9264
OlehKotov <mail.oleg456@gmail.com>
2025-10-01 19:48:54 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import { initMongoDB } from './db/initMongoDB.js';
 // import { startServer } from './server.js';
 import app from './server.js';
 
+let isDbConnected = false;
+
+export default async function handler(req, res) {
+  if (!isDbConnected) {
+    await initMongoDB();
+    isDbConnected = true;
+  }
+  return app(req, res);
+}
+
 // const bootstrap = async () => {
 //   await initMongoDB();
 //   startServer();
 // };
 
 // bootstrap();
-
-await initMongoDB();
-
-export default app;
