2900837edbd29e86e7a21fe1479fd0b7d7e6b61c
OlehKotov <mail.oleg456@gmail.com>
2025-10-02 09:46:50 +0300

    update server

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import { initMongoDB } from './db/initMongoDB.js';
-import { startServer } from './server.js';
-// import app from './server.js';
+import app from './server.js';
 
-// let isDbConnected = false;
+let isDbConnected = false;
 
-// export default async function handler(req, res) {
-//   try {
-//     if (!isDbConnected) {
-//       await initMongoDB();
-//       isDbConnected = true;
-//     }
-//     app.handle(req, res); 
-//   } catch (err) {
-//     console.error("Serverless crash:", err);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// }
+export default async function handler(req, res) {
+  try {
+    if (!isDbConnected) {
+      await initMongoDB();
+      isDbConnected = true;
+    }
+    app.handle(req, res);
+  } catch (err) {
+    console.error('Serverless crash:', err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
 
-const bootstrap = async () => {
-  await initMongoDB();
-  startServer();
-};
+// import { startServer } from './server.js';
 
-bootstrap();
+// const bootstrap = async () => {
+//   await initMongoDB();
+//   startServer();
+// };
+
+// bootstrap();
--- src/server.js
+++ src/server.js
@@ -7,39 +7,39 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
 
-// const app = express();
+const app = express();
 
-// app.use(express.json());
-// app.use(
-//   cors({
-//     origin: ['http://localhost:5173', 'https://home-mongodb-git-main-olehs-projects-15663df7.vercel.app'],
-//     credentials: true,
-//   }),
-// );
-// app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://home-mongodb-git-main-olehs-projects-15663df7.vercel.app'],
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
 
-// app.use(router);
-// app.use(notFoundHandler);
-// app.use(errorHandler);
+app.use(router);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
-// export default app;
+export default app;
 
 // const PORT = Number(getEnvVar('PORT', '3000'));
 
-export const startServer = () => {
-  const app = express();
+// export const startServer = () => {
+//   const app = express();
 
-  app.use(express.json());
+//   app.use(express.json());
 
-  app.use(
-    cors({
-      origin: ['http://localhost:5173', 'https://home-mongodb-1.onrender.com'],
-      credentials: true,
-    }),
-  );
+//   app.use(
+//     cors({
+//       origin: ['http://localhost:5173', 'https://home-mongodb-1.onrender.com'],
+//       credentials: true,
+//     }),
+//   );
 
-  app.use(cookieParser());
-  app.use(router);
-  app.use(notFoundHandler);
-  app.use(errorHandler);
-};
+//   app.use(cookieParser());
+//   app.use(router);
+//   app.use(notFoundHandler);
+//   app.use(errorHandler);
+// };
