da0047fc44af34e367d08889b5cdb57605a2b86b
OlehKotov <mail.oleg456@gmail.com>
2025-10-02 14:49:34 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://home-mongodb-git-main-olehs-projects-15663df7.vercel.app'],
+    origin: ['http://localhost:5173'],
     credentials: true,
   }),
 );
