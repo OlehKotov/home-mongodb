caea25ec8dacc76c872ecbb2cc7a0d98513f2448
OlehKotov <mail.oleg456@gmail.com>
2025-10-05 20:48:39 +0300

    updatу server.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-// import { initMongoDB } from './db/initMongoDB.js';
-// import app from './server.js';
-
-// let isDbConnected = false;
-
-// export default async function handler(req, res) {
-//   try {
-//     if (!isDbConnected) {
-//       await initMongoDB();
-//       isDbConnected = true;
-//     }
-//     app.handle(req, res);
-//   } catch (err) {
-//     console.error('Serverless crash:', err);
-//     res.status(500).json({ message: 'Internal server error' });
-//   }
-// }
-
-
 import { initMongoDB } from './db/initMongoDB.js';
-import { startServer } from './server.js';
+import app from './server.js';
 
-const bootstrap = async () => {
-  await initMongoDB();
-  startServer();
-};
+let isDbConnected = false;
 
-bootstrap();
+export default async function handler(req, res) {
+  try {
+    if (!isDbConnected) {
+      await initMongoDB();
+      isDbConnected = true;
+    }
+    app.handle(req, res);
+  } catch (err) {
+    console.error('Serverless crash:', err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+
+// import { initMongoDB } from './db/initMongoDB.js';
+// import { startServer } from './server.js';
+
+// const bootstrap = async () => {
+//   await initMongoDB();
+//   startServer();
+// };
+
+// bootstrap();
 
